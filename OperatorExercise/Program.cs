986f9fa65cbb1dc362d9b5b34923742516740ad6
Noah Moss <noahmoss30@gmail.com>
2024-08-22 12:59:26 -0500

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //For division, creat two integer variable called a and b.
+            
+
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("Please enter the radius of your circle");
+            var userInput = Consol.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaOfCircle(radius));
+        }
+
+        //define a method - which performs some functionality.  that can
+        // be used over and over again
+        public static AreaOfCircle(double radius)
+        {
+            //this is the scope of the method
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
         }
     }
 }
